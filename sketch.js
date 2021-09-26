11055c84f0b4b8069d6436c975a0e9ac3dee6358
rajdarbar802 <91432433+rajdarbar802@users.noreply.github.com>
2021-09-26 14:51:49 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var box
+function setup() {
+  createCanvas(400,400);
+   box=createSprite(200,200,50,50)
+   box.shapeColor= " red"
+}
+
+function draw() 
+{
+  background(30);
+  if (keyIsDown(UP_ARROW)) {
+    box.position.y=box.position.y-5 
+   }
+   if (keyIsDown(DOWN_ARROW)) {
+     box.position.y=box.position.y+5
+   }
+ drawSprites()
+}
+
+
+
+
